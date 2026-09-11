00747b5ff8b1a456f17b6c452934eb6ecf70ed78
bestwin13 <paulbestwin@gmail.com>
2026-09-11 23:40:45 +0530

    Update next.config.ts for export and image settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+//import type { NextConfig } from "next";
+
+//const nextConfig: NextConfig = {
+//  reactStrictMode: true,
+//};
+
+//export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
